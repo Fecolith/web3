a2b85ac0deac60109f95576cd2714b25133da964
Fecolith <87862944+Fecolith@users.noreply.github.com>
2023-04-06 09:11:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          FECOLITHS: Fertilizing the metaverse with meme poo art. Just for fun!
+          Start fertilizing web3 by minting your fecolith!
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MINT
         </a>
       </header>
     </div>
